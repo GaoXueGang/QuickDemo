dfd3438cfb8de69e7ef4958b7b35911dd49fa42d
GaoXueGang <GaoXueGang@users.noreply.github.com>
2017-08-04 10:58:22 +0800

    导入jw_loader

--- src/app/MyApp.lua
+++ src/app/MyApp.lua
@@ -1,18 +1,23 @@
-
-require("config")
-require("cocos.init")
-require("framework.init")
 require("app.init")
 
 local MyApp = class("MyApp", cc.mvc.AppBase)
 
 function MyApp:ctor()
     MyApp.super.ctor(self)
+    math.newrandomseed()
+    cc.Director:getInstance():setAnimationInterval(1 / (CONFIG_FPS_NUMBERS or 30))
+    self:init_()
+end
+
+function MyApp:init_()
 end
 
 function MyApp:run()
-    cc.FileUtils:getInstance():addSearchPath("res/")
-    self:enterScene("LoginScene")
+    if CC_DISABLE_GLOBAL then
+        -- jw.utils.dennyGlobalVariable()
+    end
+    
+    self:enterScene("LoadScene")
 end
 
 return MyApp
--- src/app/init.lua
+++ src/app/init.lua
@@ -1,4 +1,13 @@
--- 这里开始加载游戏的lua
+function __G__TRACKBACK__(errorMessage)
+    print("----------------------------------------")
+    local output = "LUA ERROR: " .. tostring(errorMessage) .. "\n"
+    output = output .. debug.traceback("", 2) .. "\n"
+    print(output)
+    print("----------------------------------------")
+end
 
-require("app.env")
 
+require("cocos.init")
+require("framework.init")
+
+require("app.const")
