2a4ce3c7349c17724a24392499281e9cc4d1211a
GaoXueGang <GaoXueGang@users.noreply.github.com>
2017-08-07 20:09:05 +0800

    脚本

--- init_build.bat
+++ init_build.bat
@@ -1,29 +1,23 @@
 @echo off
 
 set DIR=%~dp0
-
 set SCRIPTS_DIR=%DIR%.data\src
-
 set DEST_DIR=%DIR%.data
-
 set TARGET_FILE=%DEST_DIR%\init.zip
-
-set COMPILE_BIN=%QUICK_V3_ROOT%quick\bin\compile_scripts.sh 
+set COMPILE_BIN=%QUICK_V3_ROOT%quick\bin\compile_scripts.bat
 
 mkdir %DEST_DIR%\
 mkdir %SCRIPTS_DIR%\
 
 del /s /q %SCRIPTS_DIR%\*.lua
-
 if exist %TARGET_FILE% del /s /q %TARGET_FILE%
 
 xcopy %DIR%src\*.lua %SCRIPTS_DIR%\ /s /e /y
 
-
-set ENCRYPT_COMMAND=""
+rem set ENCRYPT_COMMAND=""
 rem if [ $1 ]; then
 rem    ENCRYPT_COMMAND=" -e xxtea_zip -ek $1 -es YOUR_SIGN "
 rem fi
 
 rem # 编译游戏脚本
-%COMPILE_BIN% -i %SCRIPTS_DIR% -o %TARGET_FILE% %ENCRYPT_COMMAND%
+%COMPILE_BIN% -i %SCRIPTS_DIR% -o %TARGET_FILE% 
